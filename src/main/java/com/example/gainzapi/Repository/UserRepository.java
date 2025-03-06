c7a60d0b6f5d09ca1b6cf25608806ccd18b12231
Zeus Elderfield <elderfieldzeus24@gmail.com>
2025-03-06 22:38:46 +0800

    reset: restarted app

--- src/main/java/com/example/gainzapi/Repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.gainzapi.Repository;
-
-import com.example.gainzapi.Models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
